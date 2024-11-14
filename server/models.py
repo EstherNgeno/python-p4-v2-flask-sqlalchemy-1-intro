8150b4053c131b4ec336d17ca31fc91e623cdca1
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-11-14 20:38:43 +0300

    final commit

--- server/models.py
+++ server/models.py
@@ -18,3 +18,11 @@ class Pet(db.Model):           #The Pet class is declared as a subclass of db.Mo
     id = db.Column(db.Integer, primary_key=True)    #The database table has 3 columns: id, name & species
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
